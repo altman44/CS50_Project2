f53a72ab114e8ff39c11e76f8d4d60fd1b2558f2
altman44 <48921520+altman44@users.noreply.github.com>
2020-06-24 22:16:00 -0300

    Updated load chat with other user and created class Chat

--- project2/application.py
+++ project2/application.py
@@ -13,30 +13,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+users = []
+
+# Routes
 @app.before_first_request
 def before_first_request():
-    session['users'] = list()
-
-@app.route("/")
-def index():
-    #print(session)
-    return render_template('main/home.html')
-
-@app.route('/chat', methods=['GET','POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        print(session)
-        if username == '':
-            flash("You must enter your username to login", 'danger')
-            return redirect(url_for('index'))
-        if username in session['users']:
-            flash('That username is already logged in', 'danger')
-            return redirect(url_for('index'))
-
-        print(len(session['users']))
-        session['users'].append(username)
-        print(session)
-        return render_template('logged/chat.html')
-
-    return redirect(url_for('index'))
+    session['activeUser'] = False
+    session['username'] = None
+    
+import routes.main
+import routes.chat
---
+++ project2/models/chat.py
@@ -0,0 +1,5 @@
+class Message():
+    def __init__(self, idSender, idReceiver, message):
+        self.idSender = idSender
+        self.idReceiver = idReceiver
+        self.message = message
